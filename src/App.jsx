0ca16cfe730ce4e19cf9f3b8c63e751532294c04
Milan318 <milanmandaviya7495@gmail.com>
2025-06-04 15:00:55 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Header from "./components/Header";
 import Form from "./components/Form";
 import axios from "axios";
 import Table from "./components/Table";
-import { Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+
+import { toast, ToastContainer } from "react-toastify";
 
 function App() {
-  const URL = "http://localhost:3000/product";
+  const URL = "http://localhost:3000/products";
   const [user, setUser] = useState({});
   const [users, setUsers] = useState([]);
   const [editId, setEditId] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     handleFetch();
@@ -31,29 +34,35 @@ function App() {
     e.preventDefault();
     if (editId === "") {
       await axios.post(URL, { ...user, id: String(Date.now()) });
+      toast.success("User added Successfully!", {});
     } else {
       let res = await axios.put(`${URL}/${editId}`, { ...user });
+      toast.info("User Updated Successfully!", {});
     }
     handleFetch();
     setEditId("");
     setUser({});
+    navigate("/data");
   };
 
   const handleDelete = async (id) => {
     await axios.delete(`${URL}/${id}`);
+    toast.warn("User Deleted Successfully!", {});
+
     handleFetch();
   };
   const handleEdit = (id) => {
     let user = users.find((item) => item.id === id);
     setEditId(id);
     setUser(user);
+    navigate("/form");
   };
 
   console.log(user);
 
   return (
     <>
-      <Header />
+      {/* <Header />
       <Form
         handleChange={handleChange}
         handleSubmit={handleSubmit}
@@ -61,11 +70,50 @@ function App() {
       />
       <Table
         users={users}
+        setUsers={setUsers}
         handleDelete={handleDelete}
         handleEdit={handleEdit}
       />
+     */}
+      <Header />
 
-  
+      <Routes>
+        <Route path="/" element={<Navigate to="/form" />} /> 
+        <Route
+          path="/form"
+          element={
+            <Form
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              user={user}
+            />
+          }
+        />
+        <Route
+          path="/data"
+          element={
+            <Table
+              users={users}
+              setUsers={setUsers}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
+          }
+        />
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,46 +1,74 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import DataTable from "react-data-table-component";
 
 function Table({ handleDelete, handleEdit, users }) {
+  const searchRef = useRef();
+  const [textFilter, setTextFilter] = useState("");
+
+  const usersData = users.filter((user) =>
+    user.email.toLowerCase().includes(textFilter.toLowerCase())
+  );
+
+  const columns = [
+   
+    {
+      name: "Email",
+      selector: (row) => row.email,
+      sortable: true,
+    },
+    {
+      name: "Password",
+      selector: (row) => row.password,
+      sortable: true,
+    },
+    {
+      name: "Action",
+      cell: (row) => (
+        <>
+          <button
+            className="btn btn-danger btn-sm me-2"
+            onClick={() => handleDelete(row.id)}
+          >
+            Delete
+          </button>
+          <button
+            className="btn btn-warning btn-sm"
+            onClick={() => handleEdit(row.id)}
+          >
+            Edit
+          </button>
+        </>
+      ),
+    },
+  ];
+
   return (
-    <div className="container">
+    <div className="container py-3">
       <div className="row">
-        <div className="col-md-6 mx-auto">
-          <table className="mt-5 table table-striped">
-            <thead className="table-dark">
-              <tr>
-                <th>Sr.no</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Action</th> 
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, idx) => {
-                let { email, password, id } = user;
-                return (
-                  <tr key={id}>
-                    <td>{idx + 1}</td>
-                    <td>{email}</td>
-                    <td>{password}</td>
-                    <td>
-                      <button
-                        onClick={() => handleDelete(id)}
-                        className="btn btn-danger me-2"
-                      >
-                        Delete
-                      </button>
-                      <button
-                        onClick={() => handleEdit(id)}
-                        className="btn btn-warning"
-                      >
-                        Edit
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+        <div className="col-md-8 mx-auto">
+          <div className="mb-3">
+            <input
+              ref={searchRef}
+              className="form-control"
+              type="text"
+              placeholder="Search here"
+              value={textFilter}
+              onChange={(e) => setTextFilter(e.target.value)}
+            />
+          </div>
+
+          <DataTable
+            title="User Data"
+            data={usersData}
+            columns={columns}
+            pagination
+            selectableRows
+            highlightOnHover
+            pointerOnHover
+            responsive
+            fixedHeader
+            fixedHeaderScrollHeight="300px"
+          />
         </div>
       </div>
     </div>
